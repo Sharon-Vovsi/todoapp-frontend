8964c479d6ef658c0ec3bd02b04ac977ced8f3af
Sharon-Vovsi <164954218+Sharon-Vovsi@users.noreply.github.com>
2025-11-30 20:04:39 -0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,5 @@
+import { neon } from '@netlify/neon';
+const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
+
+const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
+export default sql;
